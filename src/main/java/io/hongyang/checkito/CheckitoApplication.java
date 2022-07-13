4edcece633270a6d197ffbb2f904c243a9ab75d2
hongyang <hongyangprivate@gmail.com>
2022-07-13 22:42:33 +0800

    Initial commit

---
+++ src/main/java/io/hongyang/checkito/CheckitoApplication.java
@@ -0,0 +1,12 @@
+package io.hongyang.checkito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckitoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CheckitoApplication.class, args);
+  }
+}
---
+++ src/test/java/io/hongyang/checkito/CheckitoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hongyang.checkito;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CheckitoApplicationTests {
+
+  @Test
+  void contextLoads() {
+    CheckitoApplication.main(new String[0]);
+    Assertions.assertTrue(true);
+  }
+}
